c715350710676ac91a355d00ac04cf1914a2548c
Samyadeep Bhowmick <samyadeep98b@gmail.com>
2019-09-21 00:42:00 +0530

    Minor changes in UI

--- welcome.php
+++ welcome.php
@@ -4,14 +4,14 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<title>Welcome:Thetemplate2K00</title>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 	</head>
 	<body style="background-color: #eee;">
 
 
 
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="#">Navbar</a>
+			<a class="navbar-brand" href="welcome.php">TheTemplate2k00</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
@@ -19,9 +19,9 @@
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mr-auto">
 				<li class="nav-item active">
-					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					<a class="nav-link" href="#">About<span class="sr-only">(current)</span></a>
 				</li>
-				<li class="nav-item">
+				<li class="nav-item active">
 					<a class="nav-link" href="#">Link</a>
 				</li>
 				<li class="nav-item dropdown">
@@ -39,12 +39,19 @@
 					<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
 				</li>
 				</ul>
-				<form class="form-inline my-2 my-lg-0">
-				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-				</form>
 			</div>
 		</nav>
+
+
+
+		
+
+
+
+
+
+
+
 
 		
 		<div class="d-flex flex-row bd-highlight mb-3">
@@ -81,7 +88,8 @@
 					<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 					<hr class="my-4">
 					<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-					<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+					<button type="button" class="btn btn-outline-primary btn-lg">Register</button>
+					<button type="button" class="btn btn-outline-primary btn-lg">Login</button>
 				</div>
 
 			</div>
@@ -189,6 +197,7 @@
 				2 days ago
 			</div>
 		</div>
+
 
 
 
