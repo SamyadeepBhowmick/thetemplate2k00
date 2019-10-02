7a4f230ab768f4eab606732df270375a9e8a388e
Samyadeep Bhowmick <samyadeep98b@gmail.com>
2019-10-03 02:59:16 +0530

    Applying GoogleMap and changes in navbar on every page

--- events.php
+++ events.php
@@ -11,34 +11,29 @@
 
 
 
-		<nav class="navbar navbar-expand-lg navbar-light bg-info">
-			<a class="navbar-brand text-white" href="welcome.php"><b>TheTemplate2k00</b></a>
+	<nav class="navbar navbar-expand-lg navbar-light text-center" style="text-align: center;background-color: #F8F8F8;">
+			<a class="navbar-brand" href="welcome.php">TheTemplate2k00</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mr-auto">
-				<li class="nav-item active">
-					<a class="nav-link" href="#">About<span class="sr-only">(current)</span></a>
-				</li>
-				<li class="nav-item active">
-					<a class="nav-link" href="events.php">Events</a>
-				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Dropdown
-					</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-					<a class="dropdown-item" href="#">Action</a>
-					<a class="dropdown-item" href="#">Another action</a>
-					<div class="dropdown-divider"></div>
-					<a class="dropdown-item" href="#">Something else here</a>
-					</div>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-				</li>
+					<li class="nav-item active">
+						<a class="nav-link lead" href="events.php">About</a>
+					</li>
+					<li class="nav-item active lead">
+						<a class="nav-link" href="events.php">Events</a>
+					</li>
+					<li class="nav-item active lead">
+						<a class="nav-link" href="events.php">Results</a>
+					</li>
+					<li class="nav-item active lead">
+						<a class="nav-link" href="events.php">Organizers</a>
+					</li>
+					<li class="nav-item active lead">
+						<a class="nav-link" href="events.php">Sponsers</a>
+					</li>
 				</ul>
 			</div>
 		</nav>
--- welcome.php
+++ welcome.php
@@ -74,7 +74,18 @@
 
 		
 
+		
 
+		<div class="bd-highlight" style="padding:10% 10% 10% 10%;">
+			
+			<!--Google map-->
+			<div id="map-container-google-3" class="z-depth-1-half map-container-3">
+				<iframe src="https://maps.google.com/maps?q=CityCentre-2&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+					style="border:0;" allowfullscreen>
+				</iframe>
+			</div>
+			
+		</div>
 
 
 		
@@ -91,16 +102,19 @@
 			<div class="card-body text-center">
 				<h3 class="card-title">Contact Us</h3>
 			</div>
-			<div class="card-footer" style="text-align: right;">
+			<div class="card-footer" style="text-align: center;">
 				<div><h5 class="text-white">Connect With Us</h5></div>
 				<a href="#">
-					<img src="img/facebook.png" alt="..." style="width:42px;height:42px;border-radius:100%;">
+					<img src="img/facebook.png" alt="..." style="width:40px;height:40px;border-radius:100%;">
 				</a>
 				<a href="#">
-					<img src="img/whatsapp.png" alt="..." style="width:42px;height:42px;border-radius:100%;">
+					<img src="img/whatsapp.png" alt="..." style="width:40px;height:40px;border-radius:100%;">
 				</a>
 				<a href="#">
-					<img src="img/instagram.png" alt="..." style="width:42px;height:42px;border-radius:100%;">
+					<img src="img/instagram.png" alt="..." style="width:40px;height:40px;border-radius:100%;">
+				</a>
+				<a href="#">
+					<img src="img/youtube.png" alt="..." style="width:40px;height:40px;border-radius:100%;">
 				</a>
 			</div>
 		
