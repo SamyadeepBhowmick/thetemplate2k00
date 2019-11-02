f99dcf3ddb87d576c95c150d864510eea5463f28
Samyadeep Bhowmick <samyadeep98b@gmail.com>
2019-11-02 22:14:23 +0530

    Integrating CallBell external chat widget plugins

--- eventform.php
+++ eventform.php
@@ -33,6 +33,19 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+
+
+		<!-- Start of Async Callbell Code -->
+		<script>
+		window.callbellSettings = {
+			token: "TNvYKiJZzeP4XVyM5vyyqivB"
+		};
+		</script>
+		<script>
+		(function(){var w=window;var ic=w.callbell;if(typeof ic==="function"){ic('reattach_activator');ic('update',callbellSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Callbell=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://dash.callbell.eu/include/'+window.callbellSettings.token+'.js';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+		</script>
+		<!-- End of Async Callbell Code -->
+
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<title>Welcome:Thetemplate2K00</title>
